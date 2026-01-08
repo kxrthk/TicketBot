42d262182bf7d93e1a20b931f03c6c64a54114f3
Satya Karthik <satyakarthik75@gmail.com>
2026-01-09 00:56:11 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# ================= CONFIG =================
+BOT_TOKEN = "8043080195:AAE-OS5iXPxnz3UU7qvUtPj7kAeIsi-GDTQ"
+CHAT_ID = "5085317287"
+
+START_TIME = 8.5  # 8:30 AM
+END_TIME = 16.0   # 4:00 PM
+
+PREFERRED_THEATERS = [
+    "PVR Next Galleria", "INOX Ashoka One", "PVR Nexus Mall",
+    "Lulu Mall", "Viswanath", "Miraj", "Asian GPR",
+    "Bhramaramba", "Mallikarjuna", "GSM Mall", "AMB", "Prasads"
+]
+
+SITES_TO_WATCH = [
+    {
+        "url": "https://in.bookmyshow.com/movies/hyderabad/the-raja-saab/ET00383697",
+        "date_trigger": "09"
+    },
+    {
+        "url": "https://www.district.in/movies/the-raja-saab-movie-tickets-in-hyderabad-MV169449?frmtid=7f6ybwylp6m&fromdate=2026-01-09",
+        "date_trigger": None
+    }
+]
